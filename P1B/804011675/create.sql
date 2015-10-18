4788f310fbaf9deab6b42409fef65d00f23dde0c
Alvin Lim <aluhvihn@gmail.com>
2015-10-17 22:41:08 -0700

    Starting
    leggo

---
+++ P1B/804011675/create.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Movie(
+    id INT,                 # Movie ID
+    title VARCHAR(20),      # Movie title
+    year INT,               # Release year
+    rating VARCHAR(20),     # MPAA rating
+    company VARCHAR(20)     # Production company
+);
+
+CREATE TABLE Actor(
+    id INT,
+    title VARCHAR(20),
+    year INT,
+    rating VARCHAR(20),
+    company VARCHAR(20)
+);
