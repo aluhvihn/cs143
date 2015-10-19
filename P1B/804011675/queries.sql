bd9b9056b59b67fba941d91502ce9465e9fa3fd3
Alvin Lim <aluhvihn@gmail.com>
2015-10-19 01:22:21 -0700

    load.sql, second query

--- P1B/804011675/queries.sql
+++ P1B/804011675/queries.sql
@@ -1,4 +1,10 @@
-SELECT CONCAT_WS( ' ', first, last)     -- Select the first and last names of actors
+###############################################
+## TODO: FIX FIRST QUERY, CREATE THIRD QUERY ##
+###############################################
+
+# Give me the names of all the actors in the movie 'Die Another Day'. Please also make sure actor names are in this format:  <firstname> <lastname>   (seperated by single space).
+#SELECT CONCAT_WS( ' ', first, last)     -- Select the first and last names of actors
+SELECT first, last     -- Select the first and last names of actors
 FROM Actor                              -- From the table "Actor"
 WHERE id IN (                           -- Where their actor ids appear in
     SELECT aid                          -- Select actor ids of actors
@@ -8,11 +14,19 @@ WHERE id IN (                           -- Where their actor ids appear in
         FROM Movie                      -- From the table "Movie"
         WHERE title = 'Die Another Day'));      -- With the title "Die Another Day"
 
+SELECT title, id
+FROM Movie
+WHERE title = 'Die Another Day';
 -- Select the first and last names of the actors from table "Actor"
 -- who appear in movies from table "MovieActor" with the movie id matching
 -- movies from table "Movie" with the title "Die Another Day"
 
-SELECT aid, COUNT(*)
-FROM MovieActor
-GROUP BY aid
-HAVING COUNT(*) > 1
+# Give me the count of all the actors who acted in multiple movies.
+SELECT COUNT(*)
+FROM (
+    SELECT COUNT(mid)
+    FROM MovieActor
+    GROUP BY aid
+    HAVING COUNT(mid) > 1) A;
+
+# Additional Query
