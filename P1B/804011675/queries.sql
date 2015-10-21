1a51dcffafe48c7848b0c7a0cbae0043dff33081
Alvin Lim <aluhvihn@gmail.com>
2015-10-20 21:32:20 -0700

    Jennifer's portion
    Used Alvin's second query for queries.sql (it ran faster and gave same
    result)

--- P1B/804011675/queries.sql
+++ P1B/804011675/queries.sql
@@ -1,27 +1,11 @@
-###############################################
-## TODO: FIX FIRST QUERY, CREATE THIRD QUERY ##
-###############################################
+# Give me the names of all the actors in the movie 'Die Another Day'. Please also make sure actor names are in this format:  <firstname> <lastname>   (seperated by single space)
 
-# Give me the names of all the actors in the movie 'Die Another Day'. Please also make sure actor names are in this format:  <firstname> <lastname>   (seperated by single space).
-#SELECT CONCAT_WS( ' ', first, last)     -- Select the first and last names of actors
-SELECT first, last     -- Select the first and last names of actors
-FROM Actor                              -- From the table "Actor"
-WHERE id IN (                           -- Where their actor ids appear in
-    SELECT aid                          -- Select actor ids of actors
-    FROM MovieActor                     -- From the table "Movie Actor"
-    WHERE mid = (                       -- Where the movie id is
-        SELECT id                       -- Select the id of the movie
-        FROM Movie                      -- From the table "Movie"
-        WHERE title = 'Die Another Day'));      -- With the title "Die Another Day"
+SELECT CONCAT(first, ' ', last) AS `Actors in "Die Another Day"`
+FROM Actor A, MovieActor MA, Movie M
+WHERE A.id = MA.aid AND M.id = MA.mid AND M.title = 'Die Another Day';
 
-SELECT title, id
-FROM Movie
-WHERE title = 'Die Another Day';
--- Select the first and last names of the actors from table "Actor"
--- who appear in movies from table "MovieActor" with the movie id matching
--- movies from table "Movie" with the title "Die Another Day"
+# Give me the count of all the actors who acted in multiple movies
 
-# Give me the count of all the actors who acted in multiple movies.
 SELECT COUNT(*)
 FROM (
     SELECT COUNT(mid)
@@ -29,4 +13,13 @@ FROM (
     GROUP BY aid
     HAVING COUNT(mid) > 1) A;
 
-# Additional Query
+# Additional query
+# Directors who has directed more than 5 Movies
+
+SELECT CONCAT(first, ' ', last) AS `Directors with 5+ Movies`
+FROM(
+        SELECT first, last
+        FROM Director D, MovieDirector MD, Movie M
+        WHERE D.id = MD.did AND MD.mid = M.id
+        GROUP BY did
+        HAVING COUNT(mid)>5 ) AS Q3;
--- P1B/804011675/query.php
+++ P1B/804011675/query.php
@@ -3,32 +3,73 @@
     <body>
         <h1>SQL Query</h1>
         <p>Type an SQL query in the following box:</p>
-        <p>Example: "<kbd>SELECT * FROM Actor WHERE id=10;</kbd>"</p>
+        <p>Example: <kbd>SELECT * FROM Actor WHERE id=10;</kbd></p>
         <form method="GET">
             <textarea name="query" rows="8" cols="60"></textarea>
             <br>
-            <input type="submit" value="submit">
+            <input type="submit" value="submit"/>
         </form>
         <p><small>Note: tables and fields are case sensitive. All tables in Project 1B are availale.</small></p>
-
+        
         <?php
             if($_GET["query"]){
-                $db_connection = mysql_connect("localhost:1438", "cs143", "");
-                if (!$db_connection) {
-                    die("Couldn't connect: ".mysql_error());
-                }
-                mysql_select_db("TEST", $db_connection);
-                $query = mysql_real_escape_string($_GET["query"]);
-                echo "$query";
-                $result = msql_query($query, $db_connection);
-                while ($row = mysql_fetch_row($result)) {
-                    # Based on SELECT
-                    $sid = $row[0];
-                    $name = $row[1];
-                    $email = $row[2];
-                    echo "$sid, $name, $email";
-                }
-                mysql_close($db_connection);
+                    #establishing connection
+                    $db_connection = mysql_connect("localhost", "cs143", "");
+                    
+                    #basic error handling
+                    #if connection fails
+                    if(!$db_connection) {
+                            $errmsg = mysql_error($db_connection);
+                            echo "Connection failed: $errmsg <br />";
+                            exit(1);
+                    }
+
+                    #selecting database
+                    mysql_select_db( "CS143", $db_connection );
+
+                    #issuing query from input
+                    $result = mysql_query( $_GET["query"] );
+
+                    #if no matching row (tuple) from database
+                    if (mysql_num_rows($result) == 0) {
+                            echo "No result found";
+                            exit(1);
+                    }
+
+                    echo "<h3>Results from MySQL:</h3>";
+                    #retrieving results
+                    #creating table for query result
+                    echo "<table border=1><tr>";
+                    $f = 0;
+                    #get column information from query
+                    #return as object (name: column name)
+                    while ($f < mysql_num_fields($result)) {
+                            $meta = mysql_fetch_field($result, $f);
+                            echo "<td><strong>" . $meta->name . "</strong></td>";
+                            $f = $f + 1;
+                    }
+                    echo "<tr>";
+
+                    $r = 0;
+                    #get row information from query
+                    #returns numerical array of strings
+                    while ($row = mysql_fetch_row($result)) {
+                            for ($r = 0; $r < $f; $r++) {
+                                    #if column is NULL, write N/A
+                                    if ($row[$r] == NULL) {
+                                            echo "<td>N/A</td>";
+                                    }
+                                    #otherwise, write fetched value of column from the row
+                                    else {
+                                            echo "<td>" . $row[$r] . "</td>";
+                                    }
+                            }
+                            echo "</td><tr>";
+                    }
+                    echo "</tr></table>";
+
+                    #closing connection
+                    mysql_close($db_connection);
             }
         ?>
     </body>
